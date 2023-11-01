3e6966f87359ccab0f6fd38ca5f70ee6b68e15f0
Francisco Devaux <frandevaux2013@gmail.com>
2023-11-01 14:54:06 -0300

    Plotter ready

--- tp6-csp/code/board.py
+++ tp6-csp/code/board.py
@@ -1,137 +1,138 @@
-import math
-import random
+class NQueensBacktrackingSolver:
+    def __init__(self, n):
+        self.n = n
+        self.solution = None
+        self.step = 0
 
-from colorama import Fore
+    def is_safe(self, board, row, col):
+        # Función para verificar si es seguro colocar una reina en la posición (row, col)
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
 
+    def solve(self, board, row):
+        # Función recursiva para encontrar la primera solución
+        if row == self.n:
+            self.solution = list(board)
+            return
+        for col in range(self.n):
+            self.step += 1
+            if self.is_safe(board, row, col):
+                board[row] = col
+                self.solve(board, row + 1)
+                if self.solution:
+                    return
 
-class Board:
-    def __init__(self, size, board=None):
-        self.size = size
-        self.board = board or [random.randint(0, size - 1) for _ in range(size)]
-        self.conflicts = self.calculate_conflicts()
+    def find_solution(self):
+        board = [-1] * self.n
+        self.solve(board, 0)
 
-    def calculate_conflicts(self):
+    def print_solution(self):
+        # Función para imprimir la solución
+        for row in range(self.n):
+            for col in range(self.n):
+                if self.solution[row] == col:
+                    print('Q', end=' ')
+                else:
+                    print('.', end=' ')
+            print()
+        
+    def calculate_conflicts(self, board):
+        # Función para calcular la cantidad de conflictos en el tablero
         conflicts = 0
-        for i in range(self.size):
-            for j in range(i + 1, self.size):
-                if self.board[i] == self.board[j] or abs(self.board[i] - self.board[j]) == abs(i - j):
+        for row in range(self.n):
+            for col in range(self.n):
+                if board[row] == col:
+                    continue
+                if board[row] - row == col - row or \
+                   board[row] + row == col + row:
                     conflicts += 1
         return conflicts
+    def run(self):
+        self.find_solution()
+        return self.solution, self.step
+    
 
-    def move_queen(self, row, new_col):
-        self.board[row] = new_col
+class NQueensForwardCheckingSolver:
+    def __init__(self, n):
+        self.n = n
+        self.solution = None
+        self.domains = {}  # Diccionario para rastrear el dominio de cada variable
+        self.step = 0
 
-    def print_board(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.board[i] == j:
-                    print(Fore.CYAN + "Q", end=" ")
+    def is_safe(self, board, row, col):
+        # Función para verificar si es seguro colocar una reina en la posición (row, col)
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
+
+    def forward_check(self, board, row, col):
+        # Función para realizar la propagación y reducir los dominios
+        for i in range(row + 1, self.n):
+            if i in self.domains:
+                if col in self.domains[i]:
+                    self.domains[i].remove(col)
+                diagonal1 = col + (i - row)
+                if diagonal1 in self.domains[i]:
+                    self.domains[i].remove(diagonal1)
+                diagonal2 = col - (i - row)
+                if diagonal2 in self.domains[i]:
+                    self.domains[i].remove(diagonal2)
+
+    def solve(self, board, row):
+        if row == self.n:
+            self.solution = list(board)
+            return
+
+        if row not in self.domains:
+            self.domains[row] = set(range(self.n))
+
+        # Seleccionar la variable con el dominio más restrictivo (menos valores en el dominio)
+        min_domain_variable = min(self.domains, key=lambda var: len(self.domains[var]))
+
+        for col in self.domains[min_domain_variable]:
+            if self.is_safe(board, row, col):
+                board[row] = col
+                original_domains = self.domains.copy()
+                self.forward_check(board, row, col)
+                self.solve(board, row + 1)
+                if self.solution:
+                    return
+                self.domains = original_domains
+                self.step += 1
+
+    def find_solution(self):
+        board = [-1] * self.n
+        self.solve(board, 0)
+
+    def run(self):
+        self.find_solution()
+        return self.solution, self.step
+
+    def print_solution(self):
+        # Función para imprimir la solución
+        for row in range(self.n):
+            for col in range(self.n):
+                if self.solution[row] == col:
+                    print('Q', end=' ')
                 else:
-                    print(Fore.WHITE + "0", end=" ")
+                    print('.', end=' ')
             print()
-        print()
-
-
-def hill_climbing(board, max_iter):
-    current_board = board
-    current_conflicts = current_board.conflicts
-    iteration = 0
-    conflict_iteration= [(current_conflicts, iteration)]
-    while iteration < max_iter and current_conflicts > 0:
-        new_board = Board(board.size)
-        row, col = random.sample(range(board.size), 2)
-        new_board.move_queen(row, current_board.board[col])
-        new_conflicts = new_board.calculate_conflicts()
-        conflict_iteration.append((current_conflicts, iteration))
-        if new_conflicts < current_conflicts:
-            current_board = new_board
-            current_conflicts = new_conflicts
-
-        iteration += 1
-
-    return current_board, current_conflicts, iteration, conflict_iteration
-
-
-def simulated_annealing(board, max_iter, initial_temp, cooling_rate):
-    current_board = board
-    current_conflicts = current_board.conflicts
-    iteration = 0
-    conflict_iteration = [(current_conflicts, iteration)]
-    temp = initial_temp
-    while iteration < max_iter and current_conflicts > 0:
-        new_board = Board(board.size)
-        row, col = random.sample(range(board.size), 2)
-        new_board.move_queen(row, current_board.board[col])
-        new_conflicts = new_board.calculate_conflicts()
-        delta_conflicts = new_conflicts - current_conflicts
-        conflict_iteration.append((current_conflicts,iteration))
-
-        if delta_conflicts <= 0 or random.random() < math.exp(-delta_conflicts / temp):
-            current_board = new_board
-            current_conflicts = new_conflicts
-
-        aux_temp = temp * cooling_rate
-        if aux_temp != 0:
-            temp = aux_temp
-        iteration += 1
-
-    return current_board, current_conflicts, iteration, conflict_iteration
-
-def generate_initial_population(board, population_size):
-    return [Board(board.size) for _ in range(population_size)]
-
-def genetic_algorithm(board, max_iter, population_size, mutation_rate):
-    population = generate_initial_population(board, population_size)
-    iteration = 0
-    conflict_iteration = []
-    while iteration < max_iter:
-        population.sort(key=lambda x: x.conflicts)
-        conflict_iteration.append((population[0].conflicts, iteration))
-        if population[0].conflicts == 0:
-            return population[0], population[0].conflicts, iteration, conflict_iteration
-
-        new_population = []
-
-        for i in range(population_size):
-            parent1, parent2 = select_parents(population)
-            child = crossover(parent1, parent2)
-
-            if random.random() < mutation_rate:
-                row, col = random.sample(range(board.size), 2)
-                child.move_queen(row, col)
-                child.conflicts = child.calculate_conflicts()  # Actualizar aptitud
-
-            new_population.append(child)
-
-        population = new_population
-        iteration += 1
-
-    population.sort(key=lambda x: x.conflicts)
-    best_solution = population[0]
-    return best_solution, best_solution.conflicts, iteration, conflict_iteration
-
-def select_parents(population):
-    parents = []
-    tournament_candidates = random.sample(population, random.randint(2, len(population)))
-    tournament_candidates.sort(key=lambda x: x.conflicts)
-    parents.append(tournament_candidates[0])
-    parents.append(tournament_candidates[1])
-    return parents
-
-def crossover(parent1, parent2):
-    crossing_point = random.randint(0, parent1.size - 1)
-    child_board = parent1.board[:crossing_point] + parent2.board[crossing_point:]
-    return Board(parent1.size, child_board)
-## Backtracking solver
-def backtracking_solver(board: Board):
-    if board.calculate_conflicts() == 0:
-        return board
-    else:
-        for i in range(board.size):
-            for j in range(board.size):
-                if board.board[i] != j:
-                    new_board = Board(board.size, board.board[:])
-                    new_board.move_queen(i, j)
-                    if new_board.calculate_conflicts() < board.calculate_conflicts():
-                        return backtracking_solver(new_board)
-        return board
+    
+    def calculate_conflicts(self, board):
+        # Función para calcular la cantidad de conflictos en el tablero
+        conflicts = 0
+        for row in range(self.n):
+            for col in range(self.n):
+                if board[row] == col:
+                    continue
+                if board[row] - row == col - row or \
+                   board[row] + row == col + row:
+                    conflicts += 1
+        return conflicts
--- tp6-csp/code/main.py
+++ tp6-csp/code/main.py
@@ -1,18 +1,5 @@
-import random
-
-from board import Board, hill_climbing, simulated_annealing, genetic_algorithm, backtracking_solver
-
-tablero = Board(15)
-tablero.print_board()
-print(tablero.conflicts)
-result = backtracking_solver(tablero)
-result.print_board()
-print(result.calculate_conflicts())
-
-
-"""from plotter import boxPlotGenerator
+from plotter import boxPlotGenerator
 
 for i in range(1):
-    boxPlotGenerator(30, 'results_iteration_' + str(i+1) +'.csv', 'boxplot' + str(i+1) + '.png')
+    boxPlotGenerator()
 
-"""
